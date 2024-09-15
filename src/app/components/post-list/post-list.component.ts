fa454106603ae9814e46d13bfde77278233fffae
Kojoel <nanaabruquah1s@gmail.com>
2024-09-15 20:02:17 +0000

    Error handler service created

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ApiClientService } from '../../services/api-client.service';
+import { Post } from '../../models/post.model';
+import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgFor],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss'
 })
 export class PostListComponent {
 
+  posts: Post[] = [];
+
+  constructor(private apiService: ApiClientService) {}
+
+  ngOnInit() {
+    this.apiService.getPosts().subscribe({
+      next: (posts) => this.posts = posts,
+      error: (error) => console.error('Error fetching posts:', error)
+    })
+  }
 }
---
+++ src/app/services/error-handler.service.ts
@@ -0,0 +1,27 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorHandlerService {
+
+  constructor() { }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
+
+  }
+}
